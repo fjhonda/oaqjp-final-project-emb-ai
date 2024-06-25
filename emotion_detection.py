2e671b71373f76ba69d68786a92bf2d9d52407f9
Francisco Lemus <fjmorataya1@gmail.com>
2024-06-24 21:53:25 -0400

    Added emotions json object

--- emotion_detection.py
+++ emotion_detection.py
@@ -6,8 +6,17 @@ def emotion_detector(text_to_analyze):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     json = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url = url, headers = headers, json = json)
-    print(response.text)
-    return "test"
 
 
-emotion_detector("I love music")
+    #formatting the response to json
+    response_json = json.loads(response.text)
+    
+    emotions_json = {
+        "anger" : response_json["emotionPredictions"]["emotion"]["anger"]
+        "disgust" : response_json["emotionPredictions"]["emotion"]["disgust"]
+        "fear" : response_json["emotionPredictions"]["emotion"]["fear"]
+        "joy" : response_json["emotionPredictions"]["emotion"]["joy"]
+        "sadness" : response_json["emotionPredictions"]["emotion"]["sadness"]
+        "dominant_emotion" : 
+    }
+
