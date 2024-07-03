6abab50e42f8906c09eefc4b3de6327268cea19e
Francisco Lemus <fjmorataya1@gmail.com>
2024-07-02 21:24:56 -0400

    Avances hasta el punto 5

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    url= 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    json_data = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(url = url, headers = headers, json = json_data)
+
+    #formatting the response to json
+    response_json = json.loads(response.text)
+    
+    emotions_json = {
+        "anger" : response_json["emotionPredictions"][0]["emotion"]["anger"],
+        "disgust" : response_json["emotionPredictions"][0]["emotion"]["disgust"],
+        "fear" : response_json["emotionPredictions"][0]["emotion"]["fear"],
+        "joy" : response_json["emotionPredictions"][0]["emotion"]["joy"],
+        "sadness" : response_json["emotionPredictions"][0]["emotion"]["sadness"]
+    }
+
+    dominant_emotion = max(emotions_json, key=emotions_json.get)
+    emotions_json["dominant_emotion"]=dominant_emotion
+    
+    return emotions_json
+
