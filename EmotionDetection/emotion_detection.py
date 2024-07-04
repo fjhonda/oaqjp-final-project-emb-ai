4b9d090e6137dad0d65d27d4d385544bd50df9af
Francisco Lemus <fjmorataya1@gmail.com>
2024-07-04 08:47:48 -0400

    Finalizado manejo de errores en repositorio

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -14,7 +14,7 @@ def empty_response():
 def emotion_detector(text_to_analyze):
 
     if text_to_analyze == None or len(text_to_analyze)==0:
-        return empty_response
+        return empty_response()
 
     url= 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
--- server.py
+++ server.py
@@ -7,15 +7,14 @@ app = Flask(__name__)
 @app.route("/emotionDetector")
 def emotionDetector():
     textToAnalyze = request.args.get('textToAnalyze')
-    print(textToAnalyze)
+    
     if textToAnalyze == None:
         return render_template("index.html")
     
-    if textToAnalyze:
-        json_emotions=emotion_detector(textToAnalyze)
-        return json_emotions
-        if json_emotions["dominant_emotion"]==None:
-            return "Invalid text! Please try again!", 400
-        return f"For the given statement, the system response is 'anger': {json_emotions['anger']}, 'disgust': {json_emotions['disgust']}, 'fear': {json_emotions['fear']}, 'joy': {json_emotions['joy']}, and 'sadness': {json_emotions['sadness']}. The dominant emotion is {json_emotions['dominant_emotion']}", 200
+    json_emotions=emotion_detector(textToAnalyze)
+    
+    if json_emotions["dominant_emotion"]==None:
+        return "Invalid text! Please try again!", 200
+    return f"For the given statement, the system response is 'anger': {json_emotions['anger']}, 'disgust': {json_emotions['disgust']}, 'fear': {json_emotions['fear']}, 'joy': {json_emotions['joy']}, and 'sadness': {json_emotions['sadness']}. The dominant emotion is {json_emotions['dominant_emotion']}", 200
 
 app.run(port=5000, debug=True)
