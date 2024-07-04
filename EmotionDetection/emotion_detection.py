11855a80b015cbc3a4a26bb4d3b61e26e33239a8
Francisco Lemus <fjmorataya1@gmail.com>
2024-07-03 22:02:39 -0400

    Agregado codigo para manejar errores

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,7 +1,21 @@
 import requests
 import json
 
+def empty_response():
+    return {
+        "anger" : None,
+        "disgust" : None,
+        "fear" : None,
+        "joy" : None,
+        "sadness" : None,
+        "dominant_emotion": None
+    }
+
 def emotion_detector(text_to_analyze):
+
+    if text_to_analyze == None or len(text_to_analyze)==0:
+        return empty_response
+
     url= 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     json_data = { "raw_document": { "text": text_to_analyze } }
